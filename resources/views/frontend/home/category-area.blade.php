2d0f609507d570c63405465aa1982fb5ec58c55c
salahaldain-abduljalil <slahaldynalkhlydy44@gmail.com>
2024-06-26 17:45:02 +0300

    Customize Categories Course Frontend

--- resources/views/frontend/home/category-area.blade.php
+++ resources/views/frontend/home/category-area.blade.php
@@ -1,3 +1,7 @@
+@php
+    $category = App\Models\Category::latest()->limit(6)->get();
+@endphp
+
 <section class="category-area pb-90px">
     <div class="container">
         <div class="row align-items-center">
@@ -12,84 +16,32 @@
             </div><!-- end col-lg-9 -->
             <div class="col-lg-3">
                 <div class="category-btn-box text-right">
-                    <a href="categories.html" class="btn theme-btn">All Categories <i class="la la-arrow-right icon ml-1"></i></a>
+                    <a href="categories.html" class="btn theme-btn">All Categories <i
+                            class="la la-arrow-right icon ml-1"></i></a>
                 </div><!-- end category-btn-box-->
             </div><!-- end col-lg-3 -->
         </div><!-- end row -->
         <div class="category-wrapper mt-30px">
             <div class="row">
-                <div class="col-lg-4 responsive-column-half">
-                    <div class="category-item">
-                        <img  class="cat__img lazy" src="{{asset('frontend.images')}}/img-loading.png" data-src="{{asset('frontend.images')}}/img1.jpg" alt="Category image">
-                        <div class="category-content">
-                            <div class="category-inner">
-                                <h3 class="cat__title"><a href="#">Business Study</a></h3>
-                                <p class="cat__meta">9 courses</p>
-                                <a href="#" class="btn theme-btn theme-btn-sm theme-btn-white">Explore<i class="la la-arrow-right icon ml-1"></i></a>
-                            </div>
-                        </div><!-- end category-content -->
-                    </div><!-- end category-item -->
-                </div><!-- end col-lg-4 -->
-                <div class="col-lg-4 responsive-column-half">
-                    <div class="category-item">
-                        <img  class="cat__img lazy" src="{{asset('frontend.images')}}/img-loading.png" data-src="{{asset('frontend.images')}}/img2.jpg" alt="Category image">
-                        <div class="category-content">
-                            <div class="category-inner">
-                                <h3 class="cat__title"><a href="#">Art & Design</a></h3>
-                                <p class="cat__meta">7 courses</p>
-                                <a href="#" class="btn theme-btn theme-btn-sm theme-btn-white">Explore<i class="la la-arrow-right icon ml-1"></i></a>
-                            </div>
-                        </div><!-- end category-content -->
-                    </div><!-- end category-item -->
-                </div><!-- end col-lg-4 -->
-                <div class="col-lg-4 responsive-column-half">
-                    <div class="category-item">
-                        <img  class="cat__img lazy" src="{{asset('frontend.images')}}/img-loading.png" data-src="{{asset('frontend.images')}}/img3.jpg" alt="Category image">
-                        <div class="category-content">
-                            <div class="category-inner">
-                                <h3 class="cat__title"><a href="#">Web Development</a></h3>
-                                <p class="cat__meta">8 courses</p>
-                                <a href="#" class="btn theme-btn theme-btn-sm theme-btn-white">Explore<i class="la la-arrow-right icon ml-1"></i></a>
-                            </div>
-                        </div><!-- end category-content -->
-                    </div><!-- end category-item -->
-                </div><!-- end col-lg-4 -->
-                <div class="col-lg-4 responsive-column-half">
-                    <div class="category-item">
-                        <img  class="cat__img lazy" src="{{asset('frontend.images')}}/img-loading.png" data-src="{{asset('frontend.images')}}/img4.jpg" alt="Category image">
-                        <div class="category-content">
-                            <div class="category-inner">
-                                <h3 class="cat__title"><a href="#">Health & Fitness</a></h3>
-                                <p class="cat__meta">6 courses</p>
-                                <a href="#" class="btn theme-btn theme-btn-sm theme-btn-white">Explore<i class="la la-arrow-right icon ml-1"></i></a>
-                            </div>
-                        </div><!-- end category-content -->
-                    </div><!-- end category-item -->
-                </div><!-- end col-lg-4 -->
-                <div class="col-lg-4 responsive-column-half">
-                    <div class="category-item">
-                        <img  class="cat__img lazy" src="{{asset('frontend.images')}}/img-loading.png" data-src="{{asset('frontend.images')}}/img5.jpg" alt="Category image">
-                        <div class="category-content">
-                            <div class="category-inner">
-                                <h3 class="cat__title"><a href="#">Photography</a></h3>
-                                <p class="cat__meta">7 courses</p>
-                                <a href="#" class="btn theme-btn theme-btn-sm theme-btn-white">Explore<i class="la la-arrow-right icon ml-1"></i></a>
-                            </div>
-                        </div><!-- end category-content -->
-                    </div><!-- end category-item -->
-                </div><!-- end col-lg-4 -->
-                <div class="col-lg-4 responsive-column-half">
-                    <div class="category-item">
-                        <img  class="cat__img lazy" src="{{asset('frontend.images')}}/img-loading.png" data-src="{{asset('frontend.images')}}/img6.jpg" alt="Category image">
-                        <div class="category-content">
-                            <div class="category-inner">
-                                <h3 class="cat__title"><a href="#">Mathematics</a></h3>
-                                <p class="cat__meta">8 courses</p>
-                                <a href="#" class="btn theme-btn theme-btn-sm theme-btn-white">Explore<i class="la la-arrow-right icon ml-1"></i></a>
-                            </div>
-                        </div><!-- end category-content -->
-                    </div><!-- end category-item -->
-                </div><!-- end col-lg-4 -->
+                @foreach ($category as $cat)
+                    @php
+                        $course = App\Models\Course::where('category_id', $cat->id)->get();
+                    @endphp
+                    <div class="col-lg-4 responsive-column-half">
+                        <div class="category-item">
+                            <img class="cat__img lazy" src="{{ asset($cat->image) }}"
+                                data-src="{{ asset($cat->image) }}" alt="Category image">
+                            <div class="category-content">
+                                <div class="category-inner">
+                                    <h3 class="cat__title"><a href="#">{{ $cat->category_name }}</a></h3>
+                                    <p class="cat__meta">{{ count($course) }} courses</p>
+                                    <a href="#" class="btn theme-btn theme-btn-sm theme-btn-white">Explore<i
+                                            class="la la-arrow-right icon ml-1"></i></a>
+                                </div>
+                            </div><!-- end category-content -->
+                        </div><!-- end category-item -->
+                    </div><!-- end col-lg-4 -->
+                @endforeach
             </div><!-- end row -->
         </div><!-- end category-wrapper -->
     </div><!-- end container -->
