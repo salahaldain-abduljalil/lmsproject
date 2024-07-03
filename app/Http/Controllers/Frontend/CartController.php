efdee64de1b1c0e8d75491a507f3a415088c0f86
salahaldain-abduljalil <slahaldynalkhlydy44@gmail.com>
2024-07-03 17:36:32 +0300

    Complete Checkout Page Frontend

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -8,6 +8,7 @@ use App\Models\Course;
 use Carbon\Carbon;
 use Gloudemans\Shoppingcart\Facades\Cart;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 
 class CartController extends Controller
@@ -193,6 +194,38 @@ class CartController extends Controller
 
         Session::forget('coupon');
         return response()->json(['success' => 'Coupon Remove Successfully']);
+
+    }
+
+    public function CheckoutCreate(){
+
+        if (Auth::check()) {
+
+            if (Cart::total() > 0) {
+                $carts = Cart::content();
+                $cartTotal = Cart::total();
+                $cartQty = Cart::count();
+
+                return view('frontend.checkout.checkout_view',compact('carts','cartTotal','cartQty'));
+            } else{
+
+                $notification = array(
+                    'message' => 'Add At list One Course',
+                    'alert-type' => 'error'
+                );
+                return redirect()->to('/')->with($notification);
+
+            }
+
+        }else{
+
+            $notification = array(
+                'message' => 'You Need to Login First',
+                'alert-type' => 'error'
+            );
+            return redirect()->route('login')->with($notification);
+
+        }
 
     }
 }
