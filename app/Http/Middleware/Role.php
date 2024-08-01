b56e1cab5cdf79d7fc8dfbf1be73932834e9a831
salahaldain-abduljalil <slahaldynalkhlydy44@gmail.com>
2024-08-02 00:41:22 +0300

    Complete Protect User Admin & Instructor Dashboard

--- app/Http/Middleware/Role.php
+++ app/Http/Middleware/Role.php
@@ -13,11 +13,20 @@ class Role
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next,$roles): Response
+    public function handle(Request $request, Closure $next,$role): Response
     {
+        $userRole = $request->user()->role;
 
-        if($request->user()->role !== $roles){
-            return redirect('dashboard');
+        if ($userRole === 'user' && $role !== 'user' ) {
+          return redirect('dashboard');
+        } elseif ($userRole === 'admin' && $role === 'user') {
+           return redirect('/admin/dashboard');
+        } elseif ($userRole === 'instructor' && $role === 'user') {
+           return redirect('/instructor/dashboard');
+        }elseif ($userRole === 'admin' && $role === 'instructor') {
+           return redirect('/admin/dashboard');
+        }elseif ($userRole === 'instructor' && $role === 'admin') {
+           return redirect('/instructor/dashboard');
         }
         return $next($request);
     }
